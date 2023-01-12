5c2522d1be0c280b693abad37f9ba50525874d4a
ntzwilly <ntzwilly@gmail.com>
2023-01-13 00:12:01 +0200

    add associations between users and articles

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_12_211951) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_12_215359) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -19,6 +19,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_12_211951) do
     t.text "description"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
